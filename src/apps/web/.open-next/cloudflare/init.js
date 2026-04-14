7eafcb6446d86091ee5e065ef9b6ddc14476c549
Matteo Tanzi <matteo.beatstanzi@gmail.com>
2026-04-14 18:41:55 -0400

    Fix monorepo deploy path for merged Cloudflare build

--- src/apps/web/.open-next/cloudflare/init.js
+++ src/apps/web/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1776196813036,
+    __BUILD_TIMESTAMP_MS__: 1776206394843,
     __NEXT_BASE_PATH__: "/job-board",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
